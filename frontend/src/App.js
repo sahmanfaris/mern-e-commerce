1d28370afea0e40a5c606e9c4e060e257a173dae
@farissahman1 <farissahman1@gmail.com>
2021-12-04 20:57:24 +0100

    added header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <div>
-      <h1>Bismillah</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Bismillah</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 };
