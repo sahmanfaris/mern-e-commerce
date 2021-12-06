d979235158ec45f73bc8f9cf653ff875ce22a88f
@farissahman1 <farissahman1@gmail.com>
2021-12-06 19:20:54 +0100

    configuring redux store

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,31 @@
-import express from 'express'
-import products from './data/products.js'
-import connectDB from './config/db.js'
-import dotenv from 'dotenv'
-import colors from 'colors'
+import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+import colors from "colors";
+import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API is running')
-})
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use("/api/products", productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = find(p => p._id === req.params.id)
-  res.json(product)
-})
+app.use(notFound);
 
-const PORT = process.env.PORT || 5000
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
   )
-)
+);
