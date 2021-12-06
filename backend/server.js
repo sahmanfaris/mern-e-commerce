33df6cdc4ddeeee71c7e57af396fbcf0477ab6fd
@sahmanfaris <@farissahman1@gmail.com>
2021-12-06 14:44:05 +0100

    adding models

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,33 @@
-import express from "express";
-import products from "./data/products.js";
+import express from 'express'
+import products from './data/products.js'
+import connectDB from './config/db.js'
+import dotenv from 'dotenv'
+import colors from 'colors'
 
-const app = express();
+dotenv.config()
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+connectDB()
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+const app = express()
 
-app.get("/api/products/:id", (req, res) => {
-  const product = find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.get('/', (req, res) => {
+  res.send('API is running')
+})
 
-const PORT = process.env.REACT_APP_PORT || 5000;
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.get('/api/products/:id', (req, res) => {
+  const product = find(p => p._id === req.params.id)
+  res.json(product)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
+)
